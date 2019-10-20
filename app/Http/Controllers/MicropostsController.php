6823efeeeba4bfe6a914605882925c63dcc31006
kzkzoomin <kzkzoomin@gmail.com>
2019-10-20 02:17:59 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+            ];
+        }
+        
+        return view('welcome', $data);
+    }
+    
+    // micropost投稿
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+
+        return back();
+    }
+    
+    // 投稿を削除
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        // 自分しか消せない
+        if (\Auth::id() === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return back();
+    }
+}
