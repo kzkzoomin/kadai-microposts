d77c9d49c88177f15ba0b666b5c4550900a3df10
kzkzoomin <kzkzoomin@gmail.com>
2019-10-26 16:51:07 +0000

    favorite page

--- resources/views/favorites/favorite_button.blade.php
+++ resources/views/favorites/favorite_button.blade.php
@@ -1,11 +1,11 @@
 @if (Auth::id() != $micropost->id)
     @if (Auth::user()->liked($micropost->id))
         {!! Form::open(['route' => ['favorites.unlike', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-sm"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-sm"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['favorites.like', $micropost->id]]) !!}
-            {!! Form::submit('Favourite', ['class' => "btn btn-primary btn-sm"]) !!}
+            {!! Form::submit('Favourite', ['class' => "btn btn-secondary btn-sm"]) !!}
         {!! Form::close() !!}
     @endif
 @endif
--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -4,11 +4,10 @@
     <div class="row">
         <aside class="col-sm-4">
             @include('users.card', ['user' => $user])
-            @include('user_follow.follow_button', ['user' => $user])
         </aside>
         <div class="col-sm-8">
             @include('users.navtabs', ['user' => $user])
-            @include('users.users', ['users' => $users])
+            @include('microposts.microposts', ['microposts' => $microposts])
         </div>
     </div>
 @endsection
