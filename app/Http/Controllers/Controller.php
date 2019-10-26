70de4922ed34f7222a0d937097b1df231c2b3d13
kzkzoomin <kzkzoomin@gmail.com>
2019-10-26 14:06:08 +0000

    follow,unfollow

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,12 +11,15 @@ class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     
-    // 投稿数カウント
     public function counts($user) {
         $count_microposts = $user->microposts()->count();
+        $count_followings = $user->followings()->count();
+        $count_followers = $user->followers()->count();
 
         return [
             'count_microposts' => $count_microposts,
+            'count_followings' => $count_followings,
+            'count_followers' => $count_followers,
         ];
     }
 }
